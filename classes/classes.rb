8b45b81176070fd546ef5c689cb61c81ce54d661
Amanda Porto <amandaporto@msn.com>
2015-09-28 15:42:19 -0400

    Completed

--- classes/classes.rb
+++ classes/classes.rb
@@ -1,1 +1,44 @@
-# Your code goes here!
+class Dog
+  attr_accessor :name
+
+  def initialize(name)
+    self.name = name
+  end
+
+  def bark
+    if @loud_or_soft == :loud
+      return "WOOF!"
+    else
+      return "woof!"
+    end
+  end
+
+  def bark_volume=(loud_or_soft)
+    @loud_or_soft = loud_or_soft
+  end
+
+  def greet(animal)
+  end
+
+end
+
+class BigDog < Dog
+  def bark
+    return "WOOF!"
+  end
+
+  def greet(animal)
+    if animal.color == "red"
+      bark
+    end
+  end
+
+end
+
+class Lizard
+  attr_accessor :color
+
+  def initialize(color)
+    self.color = color
+  end
+end
